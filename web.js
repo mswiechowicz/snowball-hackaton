b8bb29602a9919012241b85bea8e94b1600554f9
Maciej Swiechowicz <maciej.swiechowicz@allegro.pl>
2021-12-13 12:57:47 +0100

    update

--- web.js
+++ web.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-let shot = true;
+let lastAction;
+let action;
 let prevScore = 0;
 
 app.get('/', function (req, res) {
@@ -12,21 +13,25 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-  console.log(req.body.arena.state[req.body._links.self.href]);
+  // console.log(req.body.arena.state[req.body._links.self.href]);
   const dataAboutMe = req.body.arena.state[req.body._links.self.href];
   const moves = ['T','F','L','R'];
   const runAway = ['F','F','L','P'];
 
   if(prevScore < dataAboutMe.score) {
-      res.send('T');
+      action='T';
   } else {
     if(dataAboutMe.wasHit) {
-      res.send(runAway[Math.floor(Math.random() * runAway.length)])
+      action = runAway[Math.floor(Math.random() * runAway.length)];
     }
-    res.send(moves[Math.floor(Math.random() * moves.length)]);
+    if(lastAction === 'T')
+      action = moves[Math.floor(Math.random() * moves.length)]
+    else
+      action = 'T';
   }
   prevScore = dataAboutMe.score;
-
+  lastAction = action;
+  res.send(action);
 //   res.send(moves[Math.floor(Math.random() * moves.length)]);
 });
 
