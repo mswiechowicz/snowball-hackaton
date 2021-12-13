47ec9e2bb5b08ba0cb920b6c687e5288ef8c8b95
Maciej Swiechowicz <maciej.swiechowicz@allegro.pl>
2021-12-13 14:30:11 +0100

    update

--- web.js
+++ web.js
@@ -15,6 +15,9 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
   // console.log(req.body.arena.state[req.body._links.self.href]);
   const dataAboutMe = req.body.arena.state[req.body._links.self.href];
+  const myDirection = dataAboutMe.direction;
+  const myX = dataAboutMe.x;
+  const myY = dataAboutMe.y;
   const moves = ['F','R'];
   const runAway = ['F','F','R'];
 
@@ -24,14 +27,50 @@ app.post('/', function (req, res) {
     if(dataAboutMe.wasHit) {
       action = runAway[Math.floor(Math.random() * runAway.length)];
     }
-    if(lastAction === 'T')
-      action = moves[Math.floor(Math.random() * moves.length)]
-    else
-      action = 'T';
+    action = findEnemy()
+    // if(lastAction === 'T')
+      // action = moves[Math.floor(Math.random() * moves.length)]
+    // else
+    //   action = 'T';
   }
   prevScore = dataAboutMe.score;
   lastAction = action;
   res.send(action);
+
+  function findEnemy(){
+    Object.values(req.body.arena.state).forEach(el => {
+      if(myDirection === 'N'){
+        if(myX === el.x) {
+          if(myY - 1 === el.y || myY - 2 === el.y || myY - 3 === el.y) {
+            return 'T';
+          }
+        }
+      }
+      if(myDirection === 'S'){
+        if(myX === el.x) {
+          if(myY + 1 === el.y || myY + 2 === el.y || myY + 3 === el.y) {
+            return 'T';
+          }
+        }
+      }
+      if(myDirection === 'E'){
+        if(myY === el.y) {
+          if(myX + 1 === el.x || myX + 2 === el.x || myX + 3 === el.x) {
+            return 'T';
+          }
+        }
+      }
+      if(myDirection === 'W'){
+        if(myY === el.y) {
+          if(myX - 1 === el.x || myX - 2 === el.x || myX - 3 === el.x) {
+            return 'T';
+          }
+        }
+      }
+    })
+    action = moves[Math.floor(Math.random() * moves.length)];
+    return action;
+  }
 //   res.send(moves[Math.floor(Math.random() * moves.length)]);
 });
 
