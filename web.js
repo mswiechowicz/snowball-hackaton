54c5cdf2f1bbf6dd977ed3a572757fb4d4b02541
Maciej Swiechowicz <maciej.swiechowicz@allegro.pl>
2021-12-13 11:58:37 +0100

    initial

---
+++ web.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+let counter = 0;
+let prevScore = 0;
+
+app.get('/', function (req, res) {
+  res.send('Let the battle begin!');
+});
+
+app.post('/', function (req, res) {
+  console.log(req.body.arena.state[req.body._links.self.href]);
+  const dataAboutMe = req.body.arena.state[req.body._links.self.href];
+  const moves = ['T','F','L','R'];
+  const moveOrShot = ['F','T','T','T','T'];
+  const runAway = ['F','F','L','P'];
+  if(prevScore < dataAboutMe.score) {
+      res.send('T');
+  }
+  prevScore = dataAboutMe.score;
+  if(dataAboutMe.wasHit) {
+      res.send(runAway[Math.floor(Math.random() * runAway.length)])
+  }
+    if(counter < 4) {
+        res.send(moveOrShot[Math.floor(Math.random() * moveOrShot.length)]);
+        counter++;
+    } else {
+        res.send(moves[Math.floor(Math.random() * moves.length)]);
+        counter = 0;
+    }
+//   res.send(moves[Math.floor(Math.random() * moves.length)]);
+});
+
+app.listen(process.env.PORT || 8080);
